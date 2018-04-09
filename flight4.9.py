e9bd2c3b8d8a0a6c33b169f94dbd2cc9346c2183
Austin Douglas <adouglas4312@gmail.com>
2018-04-09 20:18:14 +0000

    adding flight4.9 code

--- flight4.9.py
+++ flight4.9.py
@@ -9,43 +9,34 @@ import flight_algo_lib as fal
 from pymavlink import mavutil
 
 
-# Connect to the Vehicle
-#print('Connecting to vehicle on: %s' % connection_string)
-
 vehicle = connect('/dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00', wait_ready=True)
 vehicle.channels.overrides = {'5':None, '6':None,'3':0}
-#print "Armed: %s" % vehicle.armed
-
-##vehicle.armed = True
-
-# Get all original channel values (before override)
-##print("Channel values from RC Tx:", vehicle.channels)
-
-##time.sleep(1)
-
-##vehicle.channels.overrides = {'5':None, '6':None,'3':1500}
-#print " Ch3: %s" % vehicle.channels['3']
-##time.sleep(3)
-
-#print("Clear all overrides")
-##vehicle.channels.overrides = {}
-##vehicle.channels.overrides = {'5':None, '6':None,'3':0}
-#print(" Channel overrides: %s" % vehicle.channels.overrides) 
-
-
 fal.arm_and_takeoff(vehicle, 10)
 print("Set default/target airspeed to 10")
 vehicle.airspeed = 10
 
-print("Going towards first point for 5 seconds ...")
+# TASK ONE: test going to different GPS coordinates and waiting
 point1 = LocationGlobalRelative(30.384754, -97.730244, 10)
-fal.simple_gotoloc(vehicle, point1,5)
-
-print("Going to the second point for 5 seconds ...")
 point2 = LocationGlobalRelative(30.384462, -97.730701, 10)
+print("TASK 1: Going towards first GPS waypoint ...")
+fal.simple_gotoloc(vehicle, point1,5)
+print("TASK 1: Going to the second TASK 1 GPS waypoint ...")
 fal.simple_gotoloc(vehicle, point2,5)
 
-print("Returning to Launch")
+# TASK TWO: test going to different NED coordinates and waiting
+print("TASK 2: Going 5m N and E...")
+fal.goto(vehicle,5,5,None) 
+print("TASK 2: Going 5m S and W...")
+fal.goto(vehicle,-5,-5,None) 
+
+# TASK THREE: test using velocity to control drone
+print("TASK 3: Velocity to N and W 1m/s for 5 seconds")
+fal.send_ned_velocity(vehicle, 1, -1, 0, 5)
+print("TASK 3: Velocity to S and E 1m/s for 5 seconds")
+fal.send_ned_velocity(vehicle, -1, 1, 0, 5)
+
+# TASK FOUR: test RTL
+print("TASK 4: Returning to Launch")
 vehicle.mode = VehicleMode("RTL")
 
 #Close vehicle object before exiting script
