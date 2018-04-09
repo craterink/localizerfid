9207f78eea7c564ac7c5d75106ea3e3343006da1
Austin Douglas <adouglas4312@gmail.com>
2018-04-09 19:34:54 +0000

    cleaned up flight_algo_lib

--- connection_test.py
+++ connection_test.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
 import time
 import math
+import flight_algo_lib
 from pymavlink import mavutil
 
 
@@ -32,64 +33,20 @@ vehicle.channels.overrides = {'5':None, '6':None,'3':0}
 #print(" Channel overrides: %s" % vehicle.channels.overrides) 
 
 
-
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-
-    print("Basic pre-arm checks")
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
-
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
-
-    print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
-
-    # Wait until the vehicle reaches a safe height before processing the goto
-    #  (otherwise the command after Vehicle.simple_takeoff will execute
-    #   immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        # Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
-            print("Reached target altitude")
-            break
-        time.sleep(1)
-
-
-arm_and_takeoff(10)
+arm_and_takeoff(vehicle, 10)
 print("Set default/target airspeed to 10")
 vehicle.airspeed = 10
 
 print("Going towards first point for 5 seconds ...")
 point1 = LocationGlobalRelative(30.384754, -97.730244, 10)
-vehicle.simple_goto(point1)
-time.sleep(5)
+vehicle.goto(vehicle, point1)
 
 print("Going to the second point for 5 seconds ...")
 point2 = LocationGlobalRelative(30.384462, -97.730701, 10)
-vehicle.simple_goto(point2)
-
-
-
-time.sleep(5)
+vehicle.goto(vehicle, point2)
 
 print("Returning to Launch")
 vehicle.mode = VehicleMode("RTL")
-
-
 
 #Close vehicle object before exiting script
 #print("\nClose vehicle object")
